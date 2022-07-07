7e7e7d184dc88e4cc620a1cc196ed7ded2a2a8de
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2022-07-07 19:12:05 +0100

    added few  quotes

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let quote = [
 
     '"The way to get started is to quit talking and begin doing." - Walt Disney',
     '"The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela',
+    '"Life is what happens when you\'re busy making other plans." -John Lennon',
+    '"If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success." - James Cameron',
     '"If life were predictable it would cease to be life, and be without flavor." - Eleanor Roosevelt',
 ];
 
