b8acdf2b7ee4bf5f9dacf9a936ead0991501212e
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2022-07-07 18:53:01 +0100

    quote text display

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ let output = document.getElementById('paragraph');
 // now we need to create an array of quotes
 let quote = [
 
-    '"The way to get started is to quit talking and begin doing" -Walt Disney',
-    '"The greatest glory in living lies not in never falling, but in rising every time we fall" -Nelson Mandela',
-    '"If life were predictable it would cease to be life, and be without flavor." -Eleanor Roosevelt',
+    '"The way to get started is to quit talking and begin doing." - Walt Disney',
+    '"The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela',
+    '"If life were predictable it would cease to be life, and be without flavor." - Eleanor Roosevelt',
 ];
 
 // now we need to add the actual "event" to our button once pressed 
 btn.addEventListener('click', function() {
 
-    var randomQuote = quote[Math.floor(Math.random() *quote.length)]  // we assigning var randomquote the value of quote
+    let randomQuote = quote[Math.floor(Math.random() *quote.length)]  // we assigning var randomquote the value of quote
     output.innerHTML = randomQuote;  // this is the output where the random quote from our array will be shown
 });
